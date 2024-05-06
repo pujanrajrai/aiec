e0af83690c58c2bf8ea4675710eac4950e5af9fa
Pujan Raj Rai <70184988+pujanrajrai@users.noreply.github.com>
2024-05-06 19:57:47 +0545

    we are done here

---
+++ core/settings/local.py
@@ -0,0 +1,59 @@
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+from .base import *
+from decouple import config
+import os
+from datetime import timedelta
+import pymysql
+pymysql.install_as_MySQLdb()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-y(jg#8^pntjid441v#m1%pi(xy&qz7*62&9bgzew280*=gc@=y'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+d_name = config('d_name', default='aiec')
+d_user = config('d_user', default='root')
+d_password = config('d_password', default='pujanrajrai09')
+d_host = config('d_host', default='127.0.0.1')
+d_port = config('d_port', default='3306')
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': d_name,
+        'USER': d_user,
+        'PASSWORD': d_password,
+        'HOST': d_host,
+        'PORT': d_port,
+        'CHARSET': 'utf8',
+        'COLLATION': 'utf8_general_ci',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+APPEND_SLASH = True
